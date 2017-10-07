3edbc475fee0e971f70eccb5cc577facf356024b
Zao Soula <contact@zaosoula.fr>
2017-10-07 20:40:56 +0200

    CSV Format

--- generator.php
+++ generator.php
@@ -3,6 +3,7 @@ if(empty($_GET['mode'])){
   echo '
   <a href="?mode=json">Generate JSON</a>
   <a href="?mode=js">Generate Javascript</a>
+  <a href="?mode=csv">Generate CSV</a>
   <a href="?mode=list">Generate List</a>
   ';
 }else{
@@ -14,6 +15,12 @@ if(empty($_GET['mode'])){
     case 'js':
       echo 'var font_awesome_brand = JSON.parse(\''.json_encode($font_awesome_brand).'\');';
       break;
+    case 'csv':
+    echo 'Domain,Name,Color,Unicode<br>';
+    foreach($font_awesome_brand as $iconName => $iconData){
+      echo $iconName.','.$iconData['name'].','.$iconData['color'].','.$iconData['unicode'].'<br>';
+    }
+    break;
     case 'list':
       echo 'All supported icons ('.count($font_awesome_brand).' icons)<br><br>';
       foreach($font_awesome_brand as $iconName => $iconData){
